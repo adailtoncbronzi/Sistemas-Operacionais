f4bda4a45ff5b7ff64d74e8cd62f1d11b5472432
Feggah <gabidferreira9@gmail.com>
2019-06-04 00:25:11 -0300

    variables changed and comments added

--- thread.c
+++ thread.c
@@ -1,206 +1,199 @@
+/*
+------------------------------------------------------------------------------------------------------------------------------
+				CODIGO REFERENTE AO TRABALHO DE SISTEMAS OPERACIONAIS, MINISTRADA PELO PROFESSOR LEON GRADVOHL.
+												1o SEMESTRE DE 2019.
+
+					DESENVOLVIDO POR GABRIEL FERREIRA, JULIANA MORRONI E LARISSA BENEVIDES.
+------------------------------------------------------------------------------------------------------------------------------ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
 #include <string.h>
 
+/*
+------------------------------------------------------------------------------------------------------------------------------
+						DECLARACAO DA STRUCT QUE SERA PASSADA COMO PARAMETRO DA THREAD
+------------------------------------------------------------------------------------------------------------------------------ */
 
-
-//Teste
-typedef struct Matrix{
-    int coordx;
-    int coordy;
-    int var;
-    int N;
-   // int eu;
-    double **matrizprinc;
-    double **matrizsup;
-    double **matrizinf;
+typedef struct Matrix {
+    int coordenada_x;
+    int coordenada_y;
+    int repeticoes;
+    int ordem_matriz;
+    double ** matrizprincipal;
+    double ** matrizsuperior;
+    double ** matrizinferior;
 
 }matrix;
 
 pthread_t outrosTIDs[16];
 matrix threads_argumento[16];
 
-void *dividir(void *arg){
+/*
+------------------------------------------------------------------------------------------------------------------------------
+					FUNCAO DA THREAD QUE DIVIDE AS MATRIZES EM SUPERIOR E INFERIOR
+------------------------------------------------------------------------------------------------------------------------------ */
 
-  int m=0, col=0;
-  matrix *information = arg;
- information->matrizprinc[0][0];
-           col = information->coordy;
-          for(int lin=information->coordx; lin < information->N ; lin++)
-          {
-            while ( col < information->N)
-            {
-                if(col >= lin)
-                {
-                    information->matrizsup [lin][col] = information->matrizprinc[lin][col];
-                    information->matrizinf [lin][col] = 0;
-                   // printf("Elementos da matriz SUP %lf, eu sou a thread [%d]\n", information->matrizsup[lin][col], information->eu);
-                   // printf("Elementos da matriz INF %lf, eu sou a thread [%d]\n", information->matrizinf[lin][col], information->eu);
-                    m++;
-                    if(m >= information->var)
-                    {
-                      return 0;
-                    }
-                }
+void * dividir(void * arg) {
 
-                else
-                {
-                    information->matrizsup [lin][col] = 0;
-                    information->matrizinf [lin][col] = information->matrizprinc[lin][col];
-                   // printf("Elementos da matriz SUP %lf, eu sou a thread [%d]\n", information->matrizsup[lin][col], information->eu);
-                   // printf("Elementos da matriz INF %lf, eu sou a thread [%d]\n", information->matrizinf[lin][col], information->eu);
-                    m++;
-                    if(m >= information->var)
-                    {
-                     return 0;
-                    }
-                }
-                col++;
+    int parada = 0, col = 0;
+    matrix * information = arg;
+    col = information->coordenada_y;
+    for (int lin = information->coordenada_x; lin < information->ordem_matriz; lin++) {
+        while (col < information->ordem_matriz) {
+            if (col >= lin) {
+                information->matrizsuperior[lin][col] = information->matrizprincipal[lin][col];
+                information->matrizinferior[lin][col] = 0;
+                parada++;
+                if (parada >= information -> repeticoes)
+                    return 0;
             }
-            col = 0;
-          }
-
+            else {
+                information->matrizsuperior[lin][col] = 0;
+                information->matrizinferior[lin][col] = information->matrizprincipal[lin][col];
+                parada++;
+                if (parada >= information->repeticoes)
+                    return 0;
+            }
+            col++;
+        }
+        col = 0;
+    }
 }
-int main(int argc, char *argv[]){
-    int T = atoi(argv[2]);
-    int N = atoi(argv[1]);
-    int x, y, lin = N, col= N, resto=0; 
-    int i;
-    int pontos = 0;     // Marcador de pontos
-    double **MATRIZ, **diagsup, **inf;        
-    FILE *matriz, *diag1, *diag2; // Arquivo do campo minado
 
+int main(int argc, char * argv[]) {
 
-    matriz = fopen(argv[3], "r"); // Lê documento Matriz.txt
+/*
+------------------------------------------------------------------------------------------------------------------------------
+											DECLARACAO DAS VARIAVEIS
+------------------------------------------------------------------------------------------------------------------------------ */
 
-    if (matriz == NULL) // Caso não consiga ler
+    int num_threads = atoi(argv[2]), ordem_matriz = atoi(argv[1]), indice_x, indice_y, lin = ordem_matriz,
+    col = ordem_matriz, resto = 0, id;
+    double **MATRIZ, **diagsup, **diaginf;
+    FILE *matriz, *diag1, *diag2;
+
+/*
+------------------------------------------------------------------------------------------------------------------------------
+										CONDICAO DE NUMERO MAXIMO DE THREADS
+------------------------------------------------------------------------------------------------------------------------------ */
+
+    if(num_threads > ordem_matriz)
+    	num_threads = ordem_matriz;
+/*
+------------------------------------------------------------------------------------------------------------------------------
+											ABERTURA DO ARQUIVO MATRIZ.TXT
+------------------------------------------------------------------------------------------------------------------------------ */
+
+    matriz = fopen(argv[3], "r");
+    if (matriz == NULL)
     {
         printf("ERRO! Impossivel abrir o arquivo matriz!\n");
-        return 1; // Finaliza com 1
+        return 1;
+    }
+/*
+------------------------------------------------------------------------------------------------------------------------------
+						ALOCACAO DINAMICA DA MATRIZ PRINCIPAL E LEITURA DOS VALORES NO ARQUIVO
+------------------------------------------------------------------------------------------------------------------------------ */
+
+    MATRIZ = (double ** ) malloc(sizeof(double * ) * (col * lin));
+    if (MATRIZ == NULL) {
+        printf("I cannot allocate memory (1)\n");
+        exit(EXIT_FAILURE);
+        return 2;
+    }
+    for (indice_x = 0; indice_x < lin; indice_x++)
+        MATRIZ[indice_x] = (double * ) malloc(col * sizeof(double));
+
+    for (indice_x = 0; indice_x < lin; indice_x++)
+        for (indice_y = 0; indice_y < col; indice_y++)
+            fscanf(matriz, "%lf", &MATRIZ[indice_x][indice_y]);
+
+/*
+------------------------------------------------------------------------------------------------------------------------------
+				ALOCACAO DINAMICA DAS MATRIZES AUXILIARES (MATRIZ DIAGONAL SUPERIOR E DIAGONAL INFERIOR)
+------------------------------------------------------------------------------------------------------------------------------ */
+
+    diagsup = (double ** ) malloc((col * lin) * sizeof(double * ));
+    if (diagsup == NULL) {
+        printf("I cannot allocate memory (2)\n");
+        exit(EXIT_FAILURE);
+        return 2;
+    }
+    for (indice_x = 0; indice_x < lin; indice_x++)
+        diagsup[indice_x] = (double * ) malloc(col * sizeof(double));
+
+    diaginf = (double ** ) malloc((col * lin) * sizeof(double * ));
+    if (diaginf == NULL) {
+        printf("I cannot allocate memory (2)\n");
+        exit(EXIT_FAILURE);
+        return 2;
     }
 
+    for (indice_x = 0; indice_x < lin; indice_x++)
+        diaginf[indice_x] = (double * ) malloc(col * sizeof(double));
+/*
+------------------------------------------------------------------------------------------------------------------------------
+						DEFININDO OS VALORES DAS VARIAVEIS DA STRUCT E CRIACAO DA THREAD
+------------------------------------------------------------------------------------------------------------------------------ */
 
-
-    MATRIZ = (double**)malloc(sizeof(double *)*(col*lin));	
-   if (MATRIZ == NULL)
- 	{
-     printf("I cannot allocate memory 1\n");
-     exit(EXIT_FAILURE);
-     return 2;
- 	} 
- 	for (x = 0; x < lin; x++){ //Percorre as linhas do Vetor de Ponteiros
-       MATRIZ[x] = (double*) malloc(col * sizeof(double));
-   }
-
-    for(x =0; x < lin; x++){
-        for(y=0; y< col; y++){
-        //  MATRIZ [x][y] = 10;
-            fscanf(matriz, "%lf", &MATRIZ[x][y]);
-                //printf("%lf", MATRIZ[x] [y]);
-
+    for (id = 0; id < num_threads; id++) {
+        threads_argumento[id].ordem_matriz = ordem_matriz;
+        threads_argumento[id].matrizprincipal = MATRIZ;
+        threads_argumento[id].matrizsuperior = diagsup;
+        threads_argumento[id].matrizinferior = diaginf;
+        threads_argumento[id].coordenada_x = (((ordem_matriz * ordem_matriz) / num_threads) * id) / ordem_matriz;
+        threads_argumento[id].coordenada_y = (((ordem_matriz * ordem_matriz) % num_threads) * id) % ordem_matriz;
+        threads_argumento[id].repeticoes = (ordem_matriz * ordem_matriz) / num_threads;
+        if (id == num_threads - 1) {
+            resto = (ordem_matriz * ordem_matriz) % num_threads;
+            threads_argumento[id].repeticoes = ((ordem_matriz * ordem_matriz) / num_threads) + resto;
         }
-            //printf("\n");
+        pthread_create(&outrosTIDs[id], NULL, dividir, (void * ) &threads_argumento[id]);
     }
+/*
+------------------------------------------------------------------------------------------------------------------------------
+											JUNCAO DAS THREADS CRIADAS
+------------------------------------------------------------------------------------------------------------------------------ */
 
-
-
-
-diagsup = (double**)malloc((col*lin)*sizeof(double *));
-    if (diagsup == NULL)
-  {
-     printf("I cannot allocate memory 2\n");
-     exit(EXIT_FAILURE);
-     return 2;
-  }
-  for (x = 0; x < lin; x++){ //Percorre as linhas do Vetor de Ponteiros
-       diagsup[x] = (double*) malloc(col * sizeof(double));
-   }
-
-
-
-inf = (double**)malloc((col*lin)*sizeof(double *));
-    if (inf == NULL)
-  {
-     printf("I cannot allocate memory 2\n");
-     exit(EXIT_FAILURE);
-     return 2;
-  } 
-             // var = threads_argumento.var = N/T;
-              for (x = 0; x < lin; x++){ //Percorre as linhas do Vetor de Ponteiros
-                  inf[x] = (double*) malloc(col * sizeof(double));
-                }
-
-                for (i=0; i<T; i++){ // i  == ID da thread
-		              threads_argumento[i].N = N;
-                  threads_argumento[i].matrizprinc = MATRIZ;
-                  threads_argumento[i].matrizsup = diagsup;
-                  threads_argumento[i].matrizinf = inf;
-                  threads_argumento[i].coordx = (((N*N)/T)*i)/N; // calcula cordx
-                  threads_argumento[i].coordy = (((N*N)%T)*i)%N; // calcula cordy
-                  threads_argumento[i].var = (N*N)/T;
-                 // threads_argumento[i].eu = i;
-                  if(i == T-1)
-                   {
-                    resto = (N*N)%T;
-                    threads_argumento[i].var = ((N * N) / T) + resto;
-                   }
-                  pthread_create(&outrosTIDs[i], NULL, dividir,(void *)&threads_argumento[i]);
-
-                }
-                
-
-    for (i=0; i<T; i++){
-
-    pthread_join(outrosTIDs[i], NULL);
-    }
+    for (id = 0; id < num_threads; id++)
+        pthread_join(outrosTIDs[id], NULL);
+/*
+------------------------------------------------------------------------------------------------------------------------------
+									ESCREVENDO O RESULTADO DA DIVISAO NOS ARQUIVOS
+------------------------------------------------------------------------------------------------------------------------------ */
 
     diag2 = fopen("Matriz.diag2", "w");
-    if (diag2 == NULL) // Caso não consiga ler
+    if (diag2 == NULL)
     {
         printf("ERRO! Impossivel abrir o arquivo diag2!");
-        return 1; // Finaliza com 1
+        return 1;
     }
 
-
-    for(lin=0; lin < N ; lin++)
-    {
-        for(col=0 ; col < N ; col++)
-        {
-            if(col < lin)
-            {
-                inf[lin][col] = MATRIZ[lin][col];
-                fprintf(diag2, "%f ", inf[lin][col]);
-                  }
-           else
-            {
-                inf[lin][col] = 0;
-                fprintf(diag2, "%f ", inf[lin][col]);
-            }
+    for (lin = 0; lin < ordem_matriz; lin++) {
+        for (col = 0; col < ordem_matriz; col++) {
+                fprintf(diag2, "%f ", diaginf[lin][col]);
         }
         fprintf(diag2, "\n");
     }
+
     diag1 = fopen("Matriz.diag1", "w");
 
-
-    if (diag1 == NULL) // Caso não consiga ler
+    if (diag1 == NULL)
     {
         printf("ERRO! Impossivel abrir o arquivo diag1!");
-        return 1; // Finaliza com 1
+        return 1;
     }
 
-     for(lin=0; lin < N ; lin++)
-    {
-        for(col=0 ; col < N ; col++)
-        {
+    for (lin = 0; lin < ordem_matriz; lin++) {
+        for (col = 0; col < ordem_matriz; col++) {
             fprintf(diag1, "%f ", diagsup[lin][col]);
-
         }
         fprintf(diag1, "\n");
     }
 
-
-    fclose(matriz); // Fecha o documento
+    fclose(matriz);
     fclose(diag1);
     fclose(diag2);
     return 0;
